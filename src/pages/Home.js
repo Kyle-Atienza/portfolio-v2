a9a8cdaced16de708a3255c628c80827fc96e64b
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-02-20 12:41:50 +0800

    debug deployment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,16 @@ import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
 function Home() {
+  console.log(process.env.REACT_APP_AXIOS_BASE_URL);
   axios.defaults.baseURL = `${process.env.REACT_APP_AXIOS_BASE_URL}`;
 
   const [experiences, setExperiences] = useState([]);
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
+    fetch("https://kyle-portfolio-v2-backend.herokuapp.com/projects")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
     axios.get("/experiences").then((res) => setExperiences(res.data));
     axios.get("/projects").then((res) => setProjects(res.data));
   }, []);
