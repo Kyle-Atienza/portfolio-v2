3ca7ed5d4b3e0c2f53fc44c86619e992521483c6
Kyle-Atienza <atienzakylefrancis@gmail.com>
2023-01-10 22:04:49 +0800

    fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,52 +1,64 @@
-import React from 'react'
+import React from "react";
 
-import { Container, Row, Col, Button, Nav } from 'react-bootstrap'
+import { Container, Row, Col, Button, Nav } from "react-bootstrap";
 
-import portrait from "../assets/portrait.png"
-import name from "../assets/name.svg"
-import portraitMobile from "../assets/portrait-mobile.png"
+import portrait from "../assets/portrait.png";
+import name from "../assets/name.svg";
+import portraitMobile from "../assets/portrait-mobile.png";
 
 function Hero() {
-    return (
-        <Container className='d-sm-block d-flex' id='hero'>
-            <Row className='position-relative flex-grow-1'>
-                <Col xs={7} className='header-image d-sm-block d-none'>
-                    <img src={portrait} className='' alt="" />
-                </Col >
-                <Col xs={12} className='d-sm-none d-block mobile-header-image px-0 py-5 d-flex align-items-center justify-content-center'>
-                    <img src={portraitMobile} alt="" className=''/>
-                </Col>
-                <Col md={8} xs={12} className='header-text translate-middle-y end-0'>
-                    <p className='text-md-start text-center'>Hi I'm</p>
-                    <img src={name} className='header-name w-100' alt="" />
-                    <p className='text-md-end text-center ms-sm-auto'>Im an IT student and Web Developer based in the Philippines.</p>
-                    <Button className='d-flex align-items-center mt-sm-5 mt-3 ms-sm-auto me-sm-0 ms-auto me-auto' id='resumeButton' href='https://drive.google.com/file/d/127lF4puuCGrjucNqHXXEG8IUcbukgJ-2/view?usp=sharing'>
-                        View Resume
-                        <ion-icon name="chevron-forward-outline"></ion-icon>
-                    </Button>
-                </Col>
-                <Col xs={1} className='socials mb-lg-0 mb-sm-5'>
-                    <Nav className='gap-sm-5 gap-3'>
-                        <Nav.Item>
-                            <Nav.Link href='https://www.linkedin.com/in/kyle-atienza-70b60b194/'>
-                                <ion-icon name="logo-linkedin"></ion-icon>
-                            </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href='https://twitter.com/AtienzaKyle'>
-                                <ion-icon name="logo-twitter"></ion-icon>
-                            </Nav.Link>
-                        </Nav.Item> 
-                        <Nav.Item>
-                            <Nav.Link href='https://github.com/Kyle-Atienza'>
-                                <ion-icon name="logo-github"></ion-icon>
-                            </Nav.Link>
-                        </Nav.Item>               
-                    </Nav>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container className="d-sm-block d-flex" id="hero">
+      <Row className="position-relative flex-grow-1">
+        <Col xs={7} className="header-image d-sm-block d-none">
+          <img src={portrait} className="" alt="" />
+        </Col>
+        <Col
+          xs={12}
+          className="d-sm-none d-block mobile-header-image px-0 py-5 d-flex align-items-center justify-content-center"
+        >
+          <img src={portraitMobile} alt="" className="" />
+        </Col>
+        <Col md={8} xs={12} className="header-text translate-middle-y end-0">
+          <p className="text-md-start text-center">Hi I'm</p>
+          <img src={name} className="header-name w-100" alt="" />
+          <p className="text-md-end text-center ms-sm-auto">
+            A creative web developer with a strong background in design and
+            coding, I have a passion for creating visually appealing and
+            user-friendly websites. With experience in a variety of programming
+            languages and frameworks.
+          </p>
+          <Button
+            className="d-flex align-items-center mt-sm-5 mt-3 ms-sm-auto me-sm-0 ms-auto me-auto"
+            id="resumeButton"
+            href="https://drive.google.com/file/d/127lF4puuCGrjucNqHXXEG8IUcbukgJ-2/view?usp=sharing"
+          >
+            View Resume
+            <ion-icon name="chevron-forward-outline"></ion-icon>
+          </Button>
+        </Col>
+        <Col xs={1} className="socials mb-lg-0 mb-sm-5">
+          <Nav className="gap-sm-5 gap-3">
+            <Nav.Item>
+              <Nav.Link href="https://www.linkedin.com/in/kyle-atienza-70b60b194/">
+                <ion-icon name="logo-linkedin"></ion-icon>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="https://twitter.com/AtienzaKyle">
+                <ion-icon name="logo-twitter"></ion-icon>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="https://github.com/Kyle-Atienza">
+                <ion-icon name="logo-github"></ion-icon>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,12 +3,15 @@ import { Row, Col } from "react-bootstrap";
 
 function Project({ project }) {
   return (
-    <Row className="justify-content-end" key={project.id}>
+    <Row className="justify-content-end gap-col-5" key={project.id}>
       <Col
-        md={4}
+        md={5}
         className="project-details d-flex flex-column justify-content-center"
       >
         <h3 className="title">{project.name}</h3>
+        {project.description?.map((paragraph) => {
+          return <p className="project-description">{paragraph}</p>;
+        })}
         <p className="tags d-flex gap-3 flex-wrap">
           {project.tags.map((tag, index) => {
             return (
@@ -19,18 +22,23 @@ function Project({ project }) {
           })}
         </p>
         <ul className="project-links d-flex gap-5 mt-lg-5 mt-2">
-          <li className="repo-link">
-            <a href={project.repoLink} className="d-flex align-items-center">
-              VIEW REPO
-              <ion-icon name="chevron-forward"></ion-icon>
-            </a>
-          </li>
-          <li className="site-link">
-            <a href={project.siteLink} className="d-flex align-items-center">
-              VIEW SITE
-              <ion-icon name="chevron-forward"></ion-icon>
-            </a>
-          </li>
+          {project.repoLink ? (
+            <li className="repo-link">
+              <a href={project.repoLink} className="d-flex align-items-center">
+                VIEW REPO
+                <ion-icon name="chevron-forward"></ion-icon>
+              </a>
+            </li>
+          ) : null}
+
+          {project.siteLink ? (
+            <li className="site-link">
+              <a href={project.siteLink} className="d-flex align-items-center">
+                VIEW SITE
+                <ion-icon name="chevron-forward"></ion-icon>
+              </a>
+            </li>
+          ) : null}
         </ul>
       </Col>
       <Col md={6} className="project-image p-0 bg-secondary shadow">
